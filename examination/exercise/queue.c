ec9881555df3a31990ce04473d79dbe4e3e5a344
Piyush289kumar <piyushraikwar289@gmail.com>
2024-01-15 19:39:06 +0530

    Queue Using Linked List

---
+++ examination/exercise/queue.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *link;
+};
+
+struct node *front = 0;
+struct node *rear = 0;
+
+void push(int el)
+{
+    struct node *newnode;
+    newnode = (struct node *)malloc(sizeof(struct node));
+    newnode->data = el;
+    newnode->link = 0;
+
+    if (front == 0 && rear == 0)
+    {
+        front = rear = newnode;
+    }
+    else
+    {
+        rear->link = newnode;
+        rear = newnode;
+    }
+}
+
+void pop()
+{
+    if (!front == 0)
+    {
+
+        struct node *temp = front;
+        front = front->link;
+        printf("\n\nPop Element is : %d", temp->data);
+        free(temp);
+    }
+    else
+    {
+        printf("\n\nQueue is Empty");
+    }
+}
+
+void peek()
+{
+    if (!front == 0)
+    {
+        printf("\n\nTop Most Element is : %d", front->data);
+    }
+    else
+    {
+        printf("\n\nQueue is Empty");
+    }
+}
+
+void display()
+{
+    if (!front == 0)
+    {
+
+        struct node *temp = front;
+
+        while (!temp == 0)
+        {
+            printf("\nQueue Element is : %d", temp->data);
+            temp = temp->link;
+        }
+    }
+    else
+    {
+        printf("\n\nQueue is Empty");
+    }
+}
+
+int main()
+{
+    push(5);
+    push(4);
+    push(3);
+    push(2);
+    push(1);
+    peek();
+    display();
+    pop();
+    peek();
+    display();
+    return 0;
+}
